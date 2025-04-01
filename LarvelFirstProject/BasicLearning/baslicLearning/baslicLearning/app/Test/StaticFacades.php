10ce425c836d8d9538847f6d3d8625228a76eb14
JADAVEDEEEP <155883280+JADAVEDEEEP@users.noreply.github.com>
2025-04-01 17:58:43 +0530

    Delete LarvelFirstProject directory

--- LarvelFirstProject/BasicLearning/baslicLearning/baslicLearning/app/Test/StaticFacades.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Test;
-
-class StaticFacades{
-    public function testingFacades() {
-        echo "My Fcacades Is running.";
-     }
-}
--- LarvelFirstProject/baslicLearning/resources/views/Queries.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-<h1>Deep Jadav</h1>
-</div>
-
